58c1e25c6e1020686081af680e3e15a874374e03
Patrick Pluequett <patrick.pluequett@wbscodingschool.com>
2025-10-16 15:26:11 +0200

    forms part 2

--- chapter-12/forms/src/App.jsx
+++ chapter-12/forms/src/App.jsx
@@ -1,12 +1,18 @@
 import { sleep, validate } from './utils';
-import { useState } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+
+import { useState, useActionState } from 'react';
+import { useFormStatus } from 'react-dom';
 
 function App() {
     return (
         <>
             <h1>Forms</h1>
             {/* <BasicForm /> */}
-            <FormWithAction />
+            {/* <FormWithAction /> */}
+            {/* <FormWithUseFormStatus /> */}
+            {/* <FormWithErrorHandling /> */}
+            <FormWithUseActionState />
         </>
     );
 }
@@ -113,5 +119,143 @@ function FormWithAction() {
 
             <button type='submit'>Submit</button>
         </form>
+    );
+}
+
+function FormWithUseFormStatus() {
+    async function submitAction(formData) {
+        const name = formData.get('name');
+        const email = formData.get('email');
+        const message = formData.get('message');
+
+        const validationErrors = validate({ name, email, message });
+
+        if (Object.keys(validationErrors).length === 0) {
+            await sleep(2000);
+
+            console.log('Submitted', { name, email, message });
+            alert('Form submitted successfully');
+        } else {
+            console.log(validationErrors);
+        }
+    }
+    return (
+        <form action={submitAction}>
+            <label htmlFor='name'>Name</label>
+            <input type='text' name='name' id='name' />
+
+            <label htmlFor='email'>Email</label>
+            <input type='text' name='email' id='email' />
+            <label htmlFor='message'>Message</label>
+            <textarea rows={4} name='message'></textarea>
+
+            <SubmitButton />
+        </form>
+    );
+}
+
+function SubmitButton() {
+    const { pending, data, action, method } = useFormStatus();
+    console.log({ pending });
+    return (
+        <>
+            <button type='submit' disabled={pending}>
+                {pending ? 'Submitting' : 'Submit'}
+            </button>
+        </>
+    );
+}
+
+function FormWithErrorHandling() {
+    async function submitAction(formData) {
+        throw new Error('This error was thrown from the action');
+    }
+    return (
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+            <form action={submitAction}>
+                <label htmlFor='name'>Name</label>
+                <input type='text' name='name' id='name' />
+
+                <label htmlFor='email'>Email</label>
+                <input type='text' name='email' id='email' />
+                <label htmlFor='message'>Message</label>
+                <textarea rows={4} name='message'></textarea>
+
+                <SubmitButton />
+            </form>
+        </ErrorBoundary>
+    );
+}
+
+function ErrorFallback({ error, resetErrorBoundary }) {
+    return (
+        <>
+            <p>Error</p>
+            <p>{error.message}</p>
+            <button onClick={resetErrorBoundary}>Retry</button>
+        </>
+    );
+}
+
+function FormWithUseActionState() {
+    async function submitAction(prevState, formData) {
+        const name = formData.get('name');
+        const email = formData.get('email');
+        const message = formData.get('message');
+
+        const validationErrors = validate({ name, email, message });
+
+        if (Object.keys(validationErrors).length === 0) {
+            await sleep(2000);
+
+            return {
+                error: null,
+                success: true,
+            };
+        } else {
+            return {
+                error: validationErrors,
+                success: false,
+                rawData: { name, email, message },
+            };
+        }
+    }
+
+    const [state, formAction, isPending] = useActionState(submitAction, {});
+
+    console.log(state);
+    return (
+        <>
+            {isPending && <p>Pending...</p>}
+            <form action={formAction}>
+                <label htmlFor='name'>Name</label>
+                <input
+                    type='text'
+                    name='name'
+                    id='name'
+                    defaultValue={state.rawData?.name}
+                />
+                {state.error?.name && <p>{state.error.name}</p>}
+
+                <label htmlFor='email'>Email</label>
+                <input
+                    type='text'
+                    name='email'
+                    id='email'
+                    defaultValue={state.rawData?.email}
+                />
+
+                {state.error?.email && <p>{state.error.email}</p>}
+                <label htmlFor='message'>Message</label>
+                <textarea
+                    rows={4}
+                    name='message'
+                    defaultValue={state.rawData?.message}
+                ></textarea>
+                {state.error?.message && <p>{state.error.message}</p>}
+                <SubmitButton />
+            </form>
+            {state.success && <p> Form submitted</p>}
+        </>
     );
 }
